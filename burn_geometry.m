e0dd269493accbb1746bd05ef7d6fecf32173d77
Luke Roberson <luke.roberson@colorado.edu>
2022-11-18 10:27:57 -0700

    initail changes?

--- burn_geometry.m
+++ burn_geometry.m
@@ -1,19 +1,21 @@
-function [Ab,Vb,Vc] = burn_geometry(r,h,rb)
+function [Ab,Vb,Vc] = burn_geometry(ri,ro,h,bd)
 
 %  This burnback model only assumes burning with the cylindrical 
 %  perforation of the grain.  Modify as needed for your rocket motor. 
 
-    if rb >= r % motor is burnt out
+    if bd >= ro-ri % motor is burnt out
         Ab = 0; % [m^2] 
+        Vb = 3.1416*((ri + bd)^2 - ri^2)*h; % [m^3]
+        Vc = 3.1416*(ri + bd)^2*h; % [m^3]  ignores the volume between the end of 
+        
     else % there is grain remaining
         %% BURN AREA
-        Ab = 2*3.1416*(r + rb)*h; % [m^2] total burn area for cylindrically perforated grain
+        Ab = 2*3.1416*(ri + bd)*h; % [m^2] total burn area for cylindrically perforated grain
 
         %% VOLUME of PROPELLANT CONSUMED
-        Vb = 3.1416*((r + rb)^2 - r^2)*h; % [m^3]
+        Vb = 3.1416*((ri + bd)^2 - ri^2)*h; % [m^3]
         
         %% CHAMBER VOLUME
-        Vc = 3.1416*(r + rb)^2*h; % [m^3]  ignores the volume between the end of 
-        % grain and nozzle entrance 
-        
-        end
+        Vc = 3.1416*(ri + bd)^2*h; % [m^3]  ignores the volume between the end of 
+        % grain and nozzle entrance        
+    end
