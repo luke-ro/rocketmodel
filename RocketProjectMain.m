b530a83127bb3399b644339256be96540f270922
skywalkaa <49798726+skywalkaa@users.noreply.github.com>
2022-12-02 13:41:51 -0700

    added tylers plot and T correction

--- RocketProjectMain.m
+++ RocketProjectMain.m
@@ -5,7 +5,7 @@ clear
 close all
 %% Problem 1
 %Reading in .csv file
-A = readmatrix('Thrust.csv');
+A = readmatrix('data/Thrust.csv');
 t = A(1:80,1);
 T = A(1:80,2); %erroneous data past index 80
 plot(t, T)
@@ -19,6 +19,32 @@ F = mean(A(:,2));
 deltat = A(end,1);
 Impulse = F*deltat*4.44822162%Converting from lb-s to N-s
 disp("Claimed Impulse = 50 N-s")
+
+%% remove bias from thrust Thrust
+%Reading in .csv file
+A = readmatrix('data/Static Fire 24.csv');
+T = A(:,1);
+t = A(:,2);
+t = t-t(1);
+t  =t./1000;
+i = 2;
+while i <= length(T)
+   if abs(T(i-1)-T(i)) > 0.5
+      index = i-2; 
+      i = length(T);
+   end
+   i = i + 1;
+end
+slope = (T(167)-T(96))/(t(167)-t(96));
+T(1:95) = T(1:95)-T(1);
+T(166:end) = T(166:end)-T(end);
+T(96:167) = T(96:167)-(slope.*t(96:167)-0.6693);
+
+plot(t,T);
+grid on
+title("Thrust vs time for static fire 24")
+xlabel('Time [s]')
+ylabel('Thrust [lbf]')
 
 %% Problem 2: Plugging in Values to get Ab-- done in burn_geometry
 
