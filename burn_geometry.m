644cd2e79c0c7e8fb177edfeee50bb4fd0e07022
skywalkaa <49798726+skywalkaa@users.noreply.github.com>
2022-12-02 12:19:47 -0700

    accounted for burn from ends

--- burn_geometry.m
+++ burn_geometry.m
@@ -7,12 +7,14 @@ function [Ab,Vb,Vc] = burn_geometry(ri,ro,h,bd)
         
     else % there is grain remaining
         %% BURN AREA
-        Ab = 2*3.1416*(ri + bd)*h; % [m^2] total burn area for cylindrically perforated grain
+        Ab = 2*3.1416*(ri + bd)*(h-2*bd) + pi*(ro.^2-(ri+bd).^2); % [m^2] total burn area for cylindrically perforated grain
     
     end
     
     %% VOLUME of PROPELLANT CONSUMED
-    Vb = 3.1416*((ri + bd)^2 - ri^2)*h; % [m^3]
+    Vtot = pi*ro^2*h;
+    
+    Vb = 3.1416*((ri + bd)^2 - ri^2)*(h-2*bd); % [m^3]
     
     %% CHAMBER VOLUME
-    Vc = 3.1416*(ri + bd)^2*h; % [m^3]  ignores the volume between the end of 
+    Vc = Vtot-Vb; % [m^3]  ignores the volume between the end of 
